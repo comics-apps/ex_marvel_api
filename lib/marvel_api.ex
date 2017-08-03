3c3089e8c61afc61da3ca196564873b31b011f4c
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2017-08-03 21:00:05 +0200

    Initial commit

---
+++ lib/marvel_api.ex
@@ -0,0 +1,18 @@
+defmodule MarvelApi do
+  @moduledoc """
+  Documentation for MarvelApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MarvelApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/marvel_api_test.exs
@@ -0,0 +1,8 @@
+defmodule MarvelApiTest do
+  use ExUnit.Case
+  doctest MarvelApi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
